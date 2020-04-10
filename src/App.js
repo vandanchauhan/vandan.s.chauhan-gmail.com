3719db0d5b771a322d55ee6a4b9d4a8f38054101
Vandan  Chauhan <vandanchauhan@Vandans-MacBook-Pro.local>
2020-04-10 20:19:24 +0530

    flyfin interview 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import Select from 'react-select';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+  const options = [
+    { value: 'blues', label: 'Blues' },
+    { value: 'rock', label: 'Rock' },
+    { value: 'jazz', label: 'Jazz' },
+    { value: 'orchestra', label: 'Orchestra' }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="outer-div">
+        <div className="offer-text">
+          <img src="money.png" className="money-icon" />
+          <text style={{color: '#6d747d', fontWeight: 'bold'}} >Upto 5% Cashback</text>
+          <img src="information.png" className="info-icon" />
+        </div>
+        <div className="divider"></div>
+        <div className="price-container">
+          <div className="price-text" >
+            <text className="from-style" >from </text><br/>
+            <text className="price-style" >€ 139</text>
+          </div>
+          <Select className="select-element" style={{borderRadius: '0px'}} options={options} value={'select a date'} placeholder="Select a date">
+          </Select>
+          <Button value="CHECK AVAILABILITY" className="button-check" size="lg" >CHECK AVAILABILITY</Button>
+        </div>
+        <div className="ticket-parent">
+          <div className="cut-left" >
+          </div>
+          <div className='dotted-spaced'>.</div>
+          <div className="cut-right">
+          </div>
+        </div>
+        <div className="footer">
+          <img src="logo192.png" className="icon100"/>
+          <text className="footer-style" >BEST PRICE GUARANTEED</text>
+        </div>
+      </div>
     </div>
   );
 }
